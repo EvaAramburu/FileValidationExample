4b5b87b5c30aa46074f4b9a76c008a722a60c680
EvaAramburu <evamariadguez@gmail.com>
2023-03-13 11:00:48 +0100

    initial

---
+++ API/IServices/IFileService.cs
@@ -0,0 +1,15 @@
+﻿using API.Models;
+using System.Collections.Generic;
+
+namespace API.IServices
+{
+    public interface IFileService
+    {
+        // post method 
+        int InsertFile(FileItem fileItem);
+        List<FileIdentifierModel> InsertFiles(List<FileItem> fileItemList);
+        void DeleteFile(int id);
+        FileItem GetFileById(int id);
+        List<FileItem> GetFilesByCriteria(FileSearchCriteria fileSearchCriteria);
+    }
+}
